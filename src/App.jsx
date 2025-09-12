5c187fb1039820862c67f8fd1a36bad90a27cee6
marwa atlam <marwaatlam63@gmail.com>
2025-09-12 21:04:57 +0300

    save my progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import EmptyWishList from "./Components/Pages/WishList/EmptyWishList"
+import TvShows from "./Components/Pages/TvShow/TvShow";  
 
-export default function App() {
+function App() {
   return (
-    // <div>App</div>
+    // <div style={{ backgroundColor: "#0d0d0d", minHeight: "100vh" }}>
+    //   <TvShows />
+    // </div>
     <EmptyWishList />
-  )
+  );
 }
+
+export default App;
--- src/Components/Pages/WishList/EmptyWishList.jsx
+++ src/Components/Pages/WishList/EmptyWishList.jsx
@@ -1,50 +1,14 @@
 import React from "react";
-import HeartIcon from "../../../assets/heart.svg";
-import BrokeHeart from "../../../assets/BrokeHeart.svg";
+import crackedHeart from "../../../assets/cracked heart.svg";
 import style from "./EmptyWishList.module.css";
 
 function EmptyWishList() {
   return (
-    <div className={`${style.emptyWishlistContainer}`}>
+    <div className={`${style.emptyWishlistContainer} text-center d-flex flex-column justify-content-center align-items-center`}>
+      <div className={`${style.emptyWishlistContent} `}>
 
-      {/* Header */}
-      <header
-        className={`${style.emptyWishlistHeader} d-flex justify-content-between align-items-center px-4 fixed-top w-100 `}
-      >
-        <h5 className="m-0 fw-bold">Movie App</h5>
-        <div className="d-flex align-items-center gap-1">
-          <img 
-            src={HeartIcon} 
-            alt="heart-icon" 
-            width="20"
-            height="20" 
-          />
-          <span>Favourite</span>
-        </div>
-      </header>
-       
-      {/* Title */}
-      <h3 className="fw-bold text-start px-4">
-        Watch list
-      </h3>
- 
-      {/* Content */}
-      <div
-        className={`${style.emptyWishlistContent} text-center d-flex flex-column justify-content-center align-items-center `}
-      >
-        <img
-          src={BrokeHeart}
-          alt="empty-heart"
-          width="212"
-        />
-        {/* <i class="fa-solid fa-heart-crack"></i> */}
-        <p className="mt-3 fs-4">Favourite is Empty...</p>
-
-        <button
-          className={`${style.emptyWishlistButton} btn btn-warning mt-3 px-5 w-25 rounded-3 `}
-        >
-          Back to home
-        </button>
+        <img src={crackedHeart} alt="cracked-heart" className={`${style.icon} `}/>
+        <p className={`${style.emptyText}`}>Favourite is Empty..</p>
       </div>
     </div>
   );
