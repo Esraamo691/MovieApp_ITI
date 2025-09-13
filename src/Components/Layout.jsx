83e32def23f0cae355e6978d0bb5a2891ebc5a3f
eman-samy22 <esamy7619@gmail.com>
2025-09-14 02:18:07 +0300

    Footer

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
 import Navbar from './Navbar/Navbar'
+import Footer from "./Pages/Footer/Footer"; 
 import { Outlet } from 'react-router-dom'
 
-export default function Layout() {
-  return <>
-  <Navbar/>
-  <Outlet/>
-  </>
-}
+// export default function Layout() {
+//   return <>
+//   <Navbar/>
+//   <Outlet/>
+//   </>
+// }
+
+const Layout = () => {
+  return (
+    <div 
+      className="layout" 
+      style={{ minHeight: "100vh", display: "flex", flexDirection: "column" }}
+    >
+      <Navbar />
+  
+      <main style={{ flex: 1 }}>
+        <Outlet />
+      </main>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
