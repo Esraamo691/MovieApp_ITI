9eb9f9952606571e81777bd084d12aa7da5560b4
Esraa Mohamed <em148937@gmail.com>
2025-09-17 02:58:05 +0300

    Finally edition

--- src/Components/Pages/Footer/Footer.jsx
+++ src/Components/Pages/Footer/Footer.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import styles from "./footer.module.css"; 
+import styles from "./footer.module.css";
 
 const teamMembers = [
-  { name: "Esraa Mohamed", linkedin: "https://www.linkedin.com/in/esraa-mohamed-955222320/" },
-  { name: "Eman Samy", linkedin: "https://www.linkedin.com/in/eman-samy-46566a366/" },
-  { name: "Fatma Adel", linkedin: "https://www.linkedin.com/in/FatemaAdel/" },
-  { name: "Marwa Atlam", linkedin: "https://www.linkedin.com/in/marwa-atlam-9a7369359/" },
+  {
+    name: "Esraa Mohamed",
+    linkedin: "https://www.linkedin.com/in/esraa-mohamed-955222320/",
+  },
+  {
+    name: "Eman Samy",
+    linkedin: "https://www.linkedin.com/in/eman-samy-46566a366/",
+  },
+  {
+    name: "Fatma Adel",
+    linkedin: "https://www.linkedin.com/in/fatma-adel-86aaa3382",
+  },
+  {
+    name: "Marwa Atlam",
+    linkedin: "https://www.linkedin.com/in/marwa-atlam-9a7369359/",
+  },
   { name: "Shahd Saied", linkedin: "https://www.linkedin.com/in/shahd-saied/" },
-
 ];
 
 const Footer = () => {
@@ -19,19 +30,32 @@ const Footer = () => {
           <div className="col-md-4 mb-4">
             <h5 className={styles.textMint}>MovieStan</h5>
             <p className={styles.about}>
-              A simple Movie App built with React that allows users to browse movies, search, and view detailed information. Designed with a clean UI using Context API and React Router for smooth navigation
+              A simple Movie App built with React that allows users to browse
+              movies, search, and view detailed information. Designed with a
+              clean UI using Context API and React Router for smooth navigation
             </p>
           </div>
 
           <div className="col-md-4 mb-4">
             <h5 className={styles.textMint}>Other Links</h5>
             <ul className="list-unstyled">
-              <li><Link className={styles.footerLink} to="/movies">Movies</Link></li>
-              <li><Link className={styles.footerLink} to="/tv-show">TV Shows</Link></li>
-              <li><Link className={styles.footerLink} to="/watch-list">Favourite</Link></li>
+              <li>
+                <Link className={styles.footerLink} to="/movies">
+                  Movies
+                </Link>
+              </li>
+              <li>
+                <Link className={styles.footerLink} to="/tv-show">
+                  TV Shows
+                </Link>
+              </li>
+              <li>
+                <Link className={styles.footerLink} to="/watch-list">
+                  Favourite
+                </Link>
+              </li>
             </ul>
           </div>
-
 
           <div className="col-md-4 mb-4">
             <h5 className={styles.textMint}>Our Team</h5>
@@ -54,7 +78,7 @@ const Footer = () => {
       </div>
 
       <div className={styles.footerBottom}>
-         &copy; 2025 MovieStan. All rights reserved.
+        &copy; 2025 MovieStan. All rights reserved.
       </div>
     </footer>
   );
